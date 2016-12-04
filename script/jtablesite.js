8e4ea32f0320498135076f6eebcd65685dfc6ba3
郝大全 <haodaquan@shoplinq.cn>
2016-12-04 14:38:15 +0800

    模板提交
    模板提交

---
+++ script/jtablesite.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('.expandable-header').click(function (e) {
+        e.preventDefault();
+
+        var $header = $(this);
+        var $content = $header.nextAll('.expandable-content');
+
+        if ($content.is(':visible')) {
+            $content.slideUp('fast');
+        } else {
+            $content.slideDown('fast');
+        }
+    });
+});
